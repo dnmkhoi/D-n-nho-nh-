53bd84b64c2a758866d620c387d60ce10387c263
dnmkhoi <54788636+dnmkhoi@users.noreply.github.com>
2021-10-20 08:28:00 +0700

    Update TracuuHoso.js
    Cập nhật spinner Tra cứu

--- src/components/TracuuHoso.js
+++ src/components/TracuuHoso.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 import ReactJson from 'react-json-view';
+import Spinner from '../Spinner';
 
 export default function TracuuHoso(){
     var ketQua;
     const [result, setResult] = useState('');
+    let [loading, setLoading] = useState(false);
+
     async function handleSubmit(e) {
         e.preventDefault();
         var tuKhoa = document.getElementById('search').value;
         if(tuKhoa == ''){
             alert('Vui lòng nhập từ khóa');
+            setLoading(false);                   
         }else{
             console.log(tuKhoa)
             fetch("https://cors-anywhere.herokuapp.com/http://dvctt.camau.gov.vn/api/hoso/tracuuhoso?tukhoa="+tuKhoa, {
@@ -19,15 +23,19 @@ export default function TracuuHoso(){
                     (ketQua) => {
                         ketQua = JSON.stringify(JSON.parse(ketQua));
                         setResult(ketQua);
+                        setLoading(false);                   
                     },
                     (error) => {
                         alert(error);
+                        setLoading(false);                   
+
                     }
-                  )              
+                  )
         }
       }
         return (
         <section className="content">
+            <Spinner loading={loading} />
             <div className="container-fluid">
             <h3 class="tle mb-0">Tra cứu hồ sơ từ hệ thống Dịch vụ công trực tuyến Tỉnh Cà Mau<br/><span class="line"></span></h3>
             <form action="enhanced-results.html">
@@ -40,7 +48,7 @@ export default function TracuuHoso(){
                                             <input type="text" id="search" class="form-control bo-l bo-none"  placeholder="Nhập mã hồ sơ/CCCD/CMND"></input>
                                         </div>
                                         <div className="col-md-2 offset-md-1">
-                                            <input type="button" id="btnSearch" value="Tìm kiếm" onClick={handleSubmit} class="btn btn-primary" style={{height:'40px',width:'100%'}}></input>
+                                            <input type="button" id="btnSearch" value="Tìm kiếm" onClick={e => { setLoading(true); handleSubmit(e) }} style={{height:'40px',width:'100%', backgroundColor:'#2067dd', color: 'white'}}></input>
                                         </div>
                                 </div>
                             </div>
