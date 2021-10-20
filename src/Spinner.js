a3e3cd55a62ae8dbf423876338d568fee04a3f6c
dnmkhoi <54788636+dnmkhoi@users.noreply.github.com>
2021-10-20 08:30:47 +0700

    Update Spinner.js
    Cập nhật Css Spinner

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 export default function Spinner({loading}) {
+
     return (
         (loading ? 
             <div style={{"position":"fixed","width":"100vw","height":"100vh","overflow":"auto","left":"0px","top":"0px","zIndex":"5000","background":"#cbcbc18a"}}>
-                <span className="css-spinner"></span>
+                <div class="loader"></div>
             </div> : null
         )
     )
